c1cc53e28cb7f692734da15aeed8cb62c05541ec
hxngxd <76171192+hxngxd@users.noreply.github.com>
2021-12-07 19:09:25 +0700

    .

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+void solve(){
+
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.INP", "r", stdin);
+    //freopen("output.OUT", "w", stdout);
+    solve();
+    return 0;
+}
