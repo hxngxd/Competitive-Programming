731be7d0275a60715ae9ade5b6f1e5d05c845af9
hxngxd <76171192+hxngxd@users.noreply.github.com>
2021-12-17 07:33:09 +0700

    .

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,44 +1,17 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-int pldsubseq(string s){
-    int n = s.size();
-    if (n==1) return 1;
-    int dp[n][n];
-    for (int i=0;i<n;i++){
-        dp[i][i]=1;
-    }
-    int i=0, t=1, j=i+t;
-    while (true){
-        if (s[i]==s[j]){
-            dp[i][j] = (j==i+1) ? 2 : dp[i+1][j-1] + 2;
-        }
-        else{
-            dp[i][j] = max(dp[i][j-1], dp[i+1][j]);
-        }
-        if (j==n){
-            t++;
-            i=0;
-            j=i+t;
-            continue;
-        }
-        if (i==0 && j==n-1) break;
-        i++; j++;
-    }
-    for (int t=1;t<n;t++){
-        for (int i=0;i<n-1;i++){
-            dp[i][i+t]=2;
-        }
-    }
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }
-    return dp[0][n-1];
-}
 int main(){
-    cout << pldsubseq("BABCBAB");
+    vector<int> nums{1, 4, 2, 10, 23, 3, 1, 0, 20};
+    int k = 3;
+    int cur_sum = accumulate(nums.begin(), nums.begin()+k, 0);
+    int best = INT_MIN;
+    for (int i=1;i<=nums.size()-k;i++){
+        cur_sum -= nums[i-1];
+        cur_sum += nums[i+k-1];
+        cout << cur_sum << endl;
+        best = max(best, cur_sum);
+    }
+    cout << best;
     return 0;
 }
