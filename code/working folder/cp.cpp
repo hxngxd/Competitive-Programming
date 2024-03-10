3a7ad39c0621ea35ea49c644d3e4bbf8d56c1669
Nguyen Tuong Hung <141505121+hxngxd@users.noreply.github.com>
2024-03-10 10:57:40 +0700

    .

--- code/working folder/cp.cpp
+++ code/working folder/cp.cpp
@@ -1,12 +1,90 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-typedef long long ll;
-#define nl '\n'
-#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-int main()
-{
-	fast_io
-	cout << "HELO WORLD";
-	return 0;
+
+
+// template <class T>
+// class SinglyLinkedList{
+// private:
+//     Node<T> * head;
+//     Node<T> * tail;
+//     int size;
+// public:
+//     SinglyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+//     void push_front(T data){
+//         Node<T> * newNode = new Node<T>(data, head);
+//         head = newNode;
+//         if (!tail) tail = newNode;
+//         size++;
+//     }
+    
+//     void push_back(T data){
+//         Node<T> * newNode = new Node<T>(data, nullptr);
+//         if (!head){
+//             head = tail = newNode;
+//         }
+//         else{
+//             tail->next = newNode;
+//             tail = tail->next;
+//         }
+//         size++;
+//     }
+
+//     void pop_front(){
+//         if (!head) return;
+//         Node<T> * tmp = head;
+//         head = head->next;
+//         delete tmp;
+//         tmp = nullptr;
+//         if (!head) tail = nullptr;
+//         size--;
+//     }
+
+//     void pop_back(){
+//         if (!head) return;
+//         if (head == tail){
+//             delete head;
+//             head = tail = nullptr;
+//         }
+//         else{
+//             Node<T> * currentNode = head;
+//             while (currentNode->next != tail){
+//                 currentNode = currentNode->next;
+//             }
+//             delete tail;
+//             tail = currentNode;
+//             tail->next = nullptr;
+//         }
+//         size--;
+//     }
+
+//     void print(){
+//         Node<T> * currentNode = head;
+//         while (currentNode){
+//             cout << currentNode->data << " ";
+//             currentNode = currentNode->next;
+//         }
+//     }
+
+//     int getSize(){
+//         return size;
+//     }
+// };
+
+int main(){
+    DoublyLinkedList<float> dll;
+    for (int i=0;i<10;i++){
+        dll.push_back(i);
+        dll.push_front(i);
+    }
+    dll.print();
+    cout << endl << dll.getSize() << endl;
+
+    dll.pop_back();
+    dll.pop_front();
+    dll.pop_front();
+    dll.print();
+    cout << endl << dll.getSize() << endl;
+    return 0;
 }
--- code/working folder/test3.cpp
+++ code/working folder/test3.cpp
@@ -4,25 +4,8 @@ typedef long long ll;
 #define nl '\n'
 #define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-int main()
-{
-    // fast_io
-    srand(time(0));
-    long long x = rand()%1000000000000000000 + 1;
-    long long low = 1, high = 10000000000000000002;
-    cout << x << nl;
-    while (low <= high){
-        long long mid = (low + high)/2;
-        cout << mid << " ";
-        if (mid == x){
-            cout << "YES"; break;
-        }
-        else if (mid < x){
-            low = mid + 1;
-        }
-        else{
-            high = mid - 1;
-        }
-    }
-    return 0;
+#include "test.h"
+
+void Test(){
+    cout << "YOYOY";
 }
