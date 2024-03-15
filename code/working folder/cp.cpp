a6702acf6195d0db6456a5d29f5e780c925dbcaa
Nguyen Tuong Hung <141505121+hxngxd@users.noreply.github.com>
2024-03-15 10:24:40 +0700

    few

--- code/working folder/cp.cpp
+++ code/working folder/cp.cpp
@@ -1,2 +1,18 @@
-#include <iostream>
-#include <
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define nl '\n'
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void test(){
+    static int n = 3;
+    cout << n << endl;
+    n++;
+}
+int main()
+{
+    fast_io
+    test();
+    test();
+    return 0;
+}
