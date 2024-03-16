3fadf444cd2f91ff1deb06609e87c5055a414266
Nguyen Tuong Hung <141505121+hxngxd@users.noreply.github.com>
2024-03-16 23:09:35 +0700

    dqw

--- code/working folder/cp.cpp
+++ code/working folder/cp.cpp
@@ -4,15 +4,15 @@ typedef long long ll;
 #define nl '\n'
 #define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void test(){
-    static int n = 3;
-    cout << n << endl;
-    n++;
-}
 int main()
 {
     fast_io
-    test();
-    test();
+    ifstream file(".confige");
+    if (file.good()){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
     return 0;
 }
